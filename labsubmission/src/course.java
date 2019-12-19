29a7020018a7f90ee9474d934a5bd5d4d9268db2
Tharun <49119576+Tharunvarshanth@users.noreply.github.com>
2019-12-19 11:23:30 +0530

    jetbrains

---
+++ labsubmission/src/course.java
@@ -0,0 +1,22 @@
+package  lab;
+public class course{
+
+
+
+    public course(int x) {
+        String s;
+        if (x == 1) {
+            System.out.println("[1] SENG 11111 - Introduction to Programming");
+            s = "SENG 11111 - Introduction to Programming";
+
+        } else if (x == 2) {
+            System.out.println("[2] SENG 11112 - Fundamentals of Engineering");
+            s = "SENG 11112 - Fundamentals of Engineering";
+
+        } else if (x == 3) {
+            System.out.println("[3] SENG 11113 - Data Structures and Algorithms");
+            s = "SENG 11113 - Data Structures and Algorithms";
+        }
+    }
+
+}
